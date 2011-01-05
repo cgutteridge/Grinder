3878b11c19af0d77ba239a927bd3e73417f454b4
Christopher Gutteridge <cjg@ecs.soton.ac.uk>
2011-01-05 18:27:55 +0000

    added include files. Now back to where I was (nearly) before being so darn stupid

--- bin/grinder.pl
+++ bin/grinder.pl
@@ -16,6 +16,7 @@ use Getopt::Long;
 
 # --set x=y
 # --delineator x=y
+# --include
 
 # --verbose
 # --quiet
@@ -25,8 +26,10 @@ use Getopt::Long;
 
 my %options = ( 
 	config=>[], 	set=>[], 	delineator=>[], 
+	include=>[],
 	noise=>1, 	in=>undef, 	out=>undef, 
-	xslt=>undef, 	format=>undef,	worksheet=>undef, );
+	xslt=>undef, 	format=>undef,	worksheet=>undef, 
+);
 
 Getopt::Long::Configure("permute");
 
@@ -48,6 +51,7 @@ GetOptions(
 	'worksheet=i' => \$options{"worksheet"},
 
 	'config=s' => $options{"config"},
+	'include=s' => $options{"include"},
 	'set=s' => $options{"set"},
 	'delineator=s' => $options{"delineator"},
 ) || show_usage();
@@ -83,6 +87,7 @@ use warnings;
 
 # set = {} or [] or $
 # delineator = {} or [] or $
+# include = [] or $
 
 # in (default -)
 # out (default -)
@@ -97,14 +102,27 @@ sub new
 	my( $class, %options ) = @_;
 
 	my $self = bless { 
-		set=>{}, 
-		delineator=>{},
+		set => {}, 
+		delineator => {},
+		include => [],
 	}, $class;
 
 	# normal options
 	foreach my $opt_key ( keys %options )
 	{
 		next if( $opt_key eq "set" || $opt_key eq "delineator" );
+		if( $opt_key eq "include" ) 
+		{
+			if( ref( $options{$opt_key} ) eq "" ) 
+			{
+				push @{$self->{$opt_key}}, $options{$opt_key};
+			}
+			else
+			{
+				push @{$self->{$opt_key}}, @{$options{$opt_key}};
+			}
+			next;
+		}
 		$self->{$opt_key} = $options{$opt_key};
 	}
 
@@ -129,6 +147,10 @@ sub new
 						my( $id, $value ) = split( /=/, $right );
 						$self->{$left}->{$id} = $value;
 					}
+					elsif( $left eq "include" )
+					{
+						push @{$self->{include}}, $right;
+					}
 					elsif( !defined $options{$left} )
 					{
 						# second definition in a config over-writes
@@ -146,7 +168,7 @@ sub new
 		}
 	}
 
-	#  passed-in options overrides config, or appends to
+	#  passed-in options overrides config for hash type fields
 	foreach my $opt_key ( keys %options )
 	{
 		my $v = $options{$opt_key};
@@ -178,6 +200,7 @@ sub new
 	$self->{in} = "-" unless defined $self->{in};
 	$self->{out} = "-" unless defined $self->{out};
 	$self->{tmp_dir} = "/tmp" unless defined $self->{tmp_dir};
+	$self->{xslt_proc} = '/usr/bin/xsltproc @@XSL@@ @@XML@@' unless defined $self->{xslt_proc};
 
 	return $self;
 }
@@ -233,6 +256,17 @@ sub close_file
 	$self->message( "Closed '$filename'.", 2 );
 }
 
+sub unlink_file
+{
+	my( $self, $filename ) = @_;
+
+	if( !unlink( $filename ) ) 
+	{
+		$self->message( "Could not unlink $filename: $!", 1 );
+	}
+	$self->message( "Unlinked '$filename'.", 2 );
+}
+
 
 sub debug
 {
@@ -280,27 +314,69 @@ sub grind
 
 	my $xml_out_fh = $self->open_output_file( $xml_file );
 
+	$self->{parse} = { rows=>0, set=>{} };	
+	foreach my $k ( keys %{ $self->{set} } ) { $self->{parse}->{set}->{$k} = $self->{set}->{$k}; }
+
 	if( $self->{format} eq "excel" ) { $self->process_excel( $in_fh, $xml_out_fh ); }
 	elsif( $self->{format} eq "csv" ) { $self->process_csv( $in_fh, $xml_out_fh ); }
 	elsif( $self->{format} eq "tsv" ) { $self->process_tsv( $in_fh, $xml_out_fh ); }
 	elsif( $self->{format} eq "psv" ) { $self->process_psv( $in_fh, $xml_out_fh ); }
 	else { $self->error( "Unknown format '".$self->{format}."'" ); }
 
-	$self->close_file( $xml_out_fh, $xml_file );
-	$self->close_file( $in_fh, $self->{in} );
-
 	#  TODO:csv
 	# TODO:psv
 	# TODO:tsv
 
+
+	# Output end of XML file
+	print { $xml_out_fh } <<END;
+</grinder-data>
+END
+	$self->close_file( $xml_out_fh, $xml_file );
+	$self->close_file( $in_fh, $self->{in} );
+
+
+
+	# If no XSLT then we are done
+	if( !defined $self->{xslt} || $self->{xslt} eq "" )
+	{
+		exit;
+	}
+
+
+	# Read include files
+	my @lines = ();
+	foreach my $inc_file ( @{$self->{include}} )
+	{
+		my $inc_fh = $self->open_input_file( $inc_file );
+		while( my $line = readline( $inc_fh ) ) { push @lines, $line; }
+		$self->close_file( $inc_fh, $inc_file );
+	}
+	my $include = join( '', @lines );
+
+
+	# Process XSLT
 	
-	#  TODO:end or
+	my $cmd = $self->{xslt_proc};
+	$cmd =~ s/\@\@XML\@\@/$xml_file/g;
+	$cmd =~ s/\@\@XSL\@\@/$self->{xslt}/g;
+
+	my $xsltproc;
+	open( $xsltproc, "-|:utf8", $cmd ) || $self->error( "Failed to exec '$cmd': $!" );
+	$self->message( "Executed '$cmd' for writing.", 2 );
+
+	my $out_fh = $self->open_output_file( $self->{out} );
 	
-	#  TODO:open tmp document
-	#  TODO:open target for RDF
-	#  TODO:spawn xslt bin
-	
-	#  TODO:insert boilerplate and stream to out
+	while( my $line = readline( $xsltproc ) )
+	{
+		$line =~ s/<!--TOP-->/$include/g;
+		print { $out_fh } $line;
+	}	
+
+	$self->close_file( $out_fh, $self->{out} );	
+	$self->close_file( $xsltproc, $cmd );	
+
+	$self->unlink_file( $xml_file );	
 }
 
 sub process_row
@@ -321,7 +397,6 @@ sub process_row
 		if( $cells->[0] eq "*SET" )
 		{
 			$self->{parse}->{set}->{$cells->[1]} = $cells->[2];
-			print "SET:!!\n";
 		}
 		else
 		{
@@ -356,16 +431,51 @@ sub process_row
 			push @{$self->{parse}->{fields}}, $cell;
 			$fields->{$cell} = 1;
 		}
+
+		# Output Header
+		print { $out } <<END;
+<?xml version="1.0" encoding='utf-8'?>
+<grinder-data xmlns="http://purl.org/openorg/grinder/ns/">
+END
+
 		return;
 	}
-	
-	print "DATA ROW\n";
-	print Dumper( $self->{parse}->{fields} );			
-	#  TODO:convert to XML 
-	# TODO:process_row
 
-	# TODO:fields
-	# TODO:data row
+	print { $out } "  <row>\n";
+	for my $i ( 0..(scalar @{$self->{parse}->{fields}} - 1 ) )
+	{
+		my $field = $self->{parse}->{fields}->[$i];
+		next if !defined $field;
+	
+		my $value = $cells->[$i];
+		next if !defined $value;
+
+		$value =~ s/^\s+//;
+		$value =~ s/\s+$//;
+
+		my @values;
+		if( $self->{delineator}->{$field} )
+		{
+			my $del = $self->{delineator}->{$field};
+			@values = split( /\s*$del\s*/, $value );
+		}
+		else
+		{
+			@values = ( $value );
+		}
+
+		foreach my $value ( @values )
+		{
+			$value =~ s/&/&amp;/g;
+			$value =~ s/>/&gt;/g;
+			$value =~ s/</&lt;/g;
+			$value =~ s/"/&quot;/g;
+			print { $out } "    <$field>$value</$field>\n";
+		}
+		$self->{parse}->{rows}++;
+	}
+	print { $out } "  </row>\n";
+	
 }
 
 sub process_excel
